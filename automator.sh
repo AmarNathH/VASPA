3d048825340bdc642931623064837c8af8c66a46
Amar Nath H <AmarNathH@users.noreply.github.com>
2017-09-16 11:05:49 +0530

    added code for setting initial values

--- automator.sh
+++ automator.sh
@@ -2,6 +2,9 @@
 echo "VASP Automator Script"
 echo "Running at Process ID:$$"
 
+echo "Initial ENCUT : $1"
+echo "Initial Kpoints : $2"
+
 if [ -e INCAR ]
 then
 echo "INCAR found"
@@ -18,3 +21,6 @@ echo "KPOINTS not found, please use generator script to make one"
 exit 0
 fi
 
+echo "Setting initial ENCUT and KPOINT values..."
+sed -i "3s/na/$1/" INCAR
+sed -i "4s/na na na/$2/" KPOINTS
