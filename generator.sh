70e55617c8f862843bc0284bd5f91c9e7b6a0b87
Amar Nath H <AmarNathH@users.noreply.github.com>
2017-09-16 10:23:00 +0530

    Made generator script
    Generator script to create INCAR and KPOINTS file

---
+++ generator.sh
@@ -0,0 +1,19 @@
+echo "Generating INCAR and KPOINTS..."
+touch INCAR
+touch KPOINTS
+
+echo "System = $1" >INCAR
+echo "ISTART = 0">>INCAR
+echo "ENCUT = na">>INCAR
+echo "EDIFF = 1e-05">>INCAR
+echo "NSW = 0">>INCAR
+echo "ICHARG = 2">>INCAR
+echo "IBRION = -1">>INCAR
+echo "INCAR generated sucessfully"
+
+echo "k-points">KPOINTS
+echo "0">>KPOINTS
+echo "Monkhorst-pack">>KPOINTS
+echo "na na na">>KPOINTS
+echo "0 0 0">>KPOINTS
+echo "KPOINTS generated successfully"
