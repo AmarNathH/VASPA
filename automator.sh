e06a5a832fa64d08c117e84684fe0b9759b8dfcd
Amar Nath H <AmarNathH@users.noreply.github.com>
2017-09-25 16:54:17 +0530

    Added provision for running loops after convergence is achived
    Provision for running the ENCUT and KPOINT loops after convergence is achieved, Immportant to make sure the convergence achieved is reliable and correct.

--- automator.sh
+++ automator.sh
@@ -22,9 +22,11 @@ read -p "Experiment Name:" EXP_N
 read -p "No of cores you want to run simulation on:" N_CORES
 read -p "Enter initial ENCUT:" ENCUT
 read -p "Enter ENCUT_step:" ENCUT_step
+read -p "No of ENCUT loops you want to run after ENCUT convergence is achieved (0 defaults in normal convergence run):" ENCUT_LOOP 
 read -p "Is the material planar(P) or Bulk(B)? " mat_state
 read -p "Enter no of initial K-points :" KPOI
 read -p "Enter KPOI_step:" KPOI_step
+read -p "No of KPOINT loops you want to run after KPOINT convergence is achieved(0 defaults in normal convergence run):" KPOI_LOOP
 
 ./generate.sh $EXP_N
 
@@ -78,6 +80,33 @@ ENERGY_I=$(echo "$ENERGY_F"|bc)
 done
 
 echo "ENCUT Converged :)"
+echo "Running ENCUT loop $ENCUT_LOOP times after convergence."
+
+#Loop to run after ENCUT convergence
+COUNT=0
+while [ "$COUNT" -ne "$ENCUT_LOOP" ]
+do
+ENCUT=$(echo "$ENCUT + $ENCUT_step"|bc)
+sed -i "3s/.*/ENCUT = $ENCUT/" INCAR #replace ENCUT line in INCAR
+echo "Running jobfile with ENCUT = $ENCUT and KPOINTS = $KPOI"
+./jobfile $N_CORES
+ENERGY_F=$(grep -Po 'E0= \K[^ ]+' out)
+ENERGY_F=$(printf "%.10f" "$ENERGY_F")
+if [ 1 -eq "$(echo "$ENERGY_F <0"|bc -l)"  ]
+then
+ENERGY_F=$(echo "0 - $ENERGY_F"|bc)
+fi
+
+delta_E=$(echo "$ENERGY_F - $ENERGY_I" |bc)
+if [ 1 -eq "$(echo "$delta_E<0"|bc -l)"  ]
+then
+delta_E=$(echo "0 - $delta_E"|bc)
+fi
+echo "ENCUT = $ENCUT, KPOINTS = $KPOI, E0 = $ENERGY_F, dE = $delta_E" >> ENCUT_data
+ENERGY_I=$(echo "$ENERGY_F"|bc)
+((COUNT++))
+done
+
 echo "Moving on to KPOINT Convergence"
 delta_E=60
 touch KPOINT_data #stores kpoint  data
@@ -114,4 +143,37 @@ ENERGY_I=$(echo "$ENERGY_F"|bc)
 done
 
 echo "KPOINT Convergence completed :)"
+echo "Running KPOINT loop $KPOI_LOOP times after convergence."
+
+#Loop to run after KPOINT convergence
+COUNT=0
+while [ "$COUNT" -ne "$KPOI_LOOP" ]
+do
+KPOI=$(echo "$KPOI + $KPOI_step"|bc)
+if [ $mat_state="P" ] #replace KPOINTS in file
+then
+sed -i "4s/.*/$KPOI $KPOI 1/" KPOINTS
+else
+sed -i "4s/.*/$KPOI $KPOI $KPOI/" KPOINTS
+fi
+echo "Running jobfile with ENCUT = $ENCUT and KPOINTS = $KPOI"
+./jobfile $N_CORES
+ENERGY_F=$(grep -Po 'E0= \K[^ ]+' out)
+ENERGY_F=$(printf "%.10f" "$ENERGY_F")
+if [ 1 -eq "$(echo "$ENERGY_F <0"|bc -l)"  ]
+then
+ENERGY_F=$(echo "0 - $ENERGY_F"|bc)
+fi
+
+delta_E=$(echo "$ENERGY_F - $ENERGY_I" |bc)
+if [ 1 -eq "$(echo "$delta_E<0"|bc -l)"  ]
+then
+delta_E=$(echo "0 - $delta_E"|bc)
+fi
+
+echo "ENCUT = $ENCUT, KPOINTS = $KPOI, E0 = $ENERGY_F, dE = $delta_E" >> KPOINT_data
+ENERGY_I=$(echo "$ENERGY_F"|bc)
+((COUNT++))
+done
+
 echo "Convergence calculations are complete, the data are stored in ENCUT_data and KPOINT_data"
