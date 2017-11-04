2291374c99b2860ff31b29d6ed7fb835d45c9e3f
Amar Nath H <AmarNathH@users.noreply.github.com>
2017-11-04 11:47:34 +0530

    Added option to add more INCAR tags

--- generate.sh
+++ generate.sh
@@ -12,6 +12,17 @@ echo "ICHARG = 2">>INCAR
 echo "IBRION = -1">>INCAR
 echo "INCAR generated sucessfully"
 
+if [ -e extra_INCAR_tags ]
+then
+echo "Found extra_INCAR_tags file"
+cat extra_INCAR_tags >> INCAR
+echo "INCAR tags added:"
+cat extra_INCAR_tags
+else
+echo "No extra_INCAR_tags file found"
+fi
+
+
 echo "k-points">KPOINTS
 echo "0">>KPOINTS
 echo "Monkhorst-pack">>KPOINTS
